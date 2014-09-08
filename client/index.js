c82879a3f615206a98ebef78e86d518636d89477
Sarah M Pearson <sarahp@me.com>
2014-09-08 15:42:26 -0500

    added create weapon and create pet

--- client/index.js
+++ client/index.js
@@ -1,17 +1,55 @@
 (function(){
   'use strict';
 
-  angular.module('mean-template', [])
-  .controller('MainController', ['$scope', '$interval', function($scope, $interval){
-    var occupations = ['Superheroes', 'Ninjas', 'Pirates', 'Vampires', 'Aliens', 'Dragons', 'Sharks with Lasers', 'Transformers', 'I am Groot'];
+  angular.module('zombie-pets', [])
+  .controller('MainController', ['$scope', function($scope){
+    $scope.title = 'Zombie Pets';
 
-    $scope.title = 'Mean Template';
-    $scope.occupation = occupations[0];
+    $scope.weapon = {};
+    $scope.weapons = [];
+    $scope.pet = {health:100, isZombie:false};
+    $scope.pets = [];
 
-    $interval(function(){
-      var rnd = Math.floor(Math.random() * occupations.length);
-      $scope.occupation = occupations[rnd];
-    }, 500);
+    $scope.player1 = null;
+    $scope.player2 = null;
+
+    $scope.addWeapon = function(){
+      $scope.weapons.push($scope.weapon);
+      $scope.weapon = {};
+      $('#name').focus();
+    };
+
+    $scope.addPet = function(){
+      var index = $scope.pet.weapon * 1;
+      $scope.pet.weapon = $scope.weapons[index];
+      $scope.pets.push($scope.pet);
+      $scope.pet = {health:100, isZombie:false};
+    };
+
+    $scope.setPlayer = function(num){
+      $scope['player' + num] = this.p;
+    };
+
+    $scope.fight = function(){
+      var x    = Math.floor(Math.random() * 2) + 1,
+          dam1 = Math.random() * $scope.player1.weapon.damage,
+          dam2 = Math.random() * $scope.player2.weapon.damage;
+      if(x === 1){
+        $scope.player1.health -= parseInt(dam1).toFixed(2);
+        $scope.player2.health -= parseInt(dam2).toFixed(2);
+      }else{
+        $scope.player2.health -= dam2;
+        $scope.player1.health -= dam1;
+      }
+    };
+
+    $scope.toggleWeapon = function(){
+      $scope.hideWeapon = !!!$scope.hideWeapon;
+    };
+
+    $scope.togglePet = function(){
+      $scope.hidePet = !!!$scope.hidePet;
+    };
   }]);
 })();
 
